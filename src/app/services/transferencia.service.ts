44f8c90a08205cb62e943ba9767cc45168cca2ca
Lorena C Silva <loricristinesilva@gmail.com>
2021-09-13 14:20:12 -0300

    aula 5

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -5,6 +5,23 @@ import { Injectable } from '@angular/core';
 })
 export class TransferenciaService {
 
-constructor() { }
+  listaTransferencia: any[];
+
+constructor() {
+  this.listaTransferencia = [];
+}
+
+get transferencias(){
+  return this.listaTransferencia;
+}
+
+adicionar(transferencia: any){
+  this.hidratar(transferencia);
+  this.listaTransferencia.push(transferencia);
+}
+
+private hidratar(transferencia: any){
+  transferencia.data = new Date();
+}
 
 }
